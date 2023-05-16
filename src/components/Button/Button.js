f7fa1d5afc1498cc6cbaa6e85d62e6fcdf2f4646
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-05-16 12:12:10 +0300

    refact modal

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,24 +1,22 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './Button.module.css';
 
-export default class Button extends Component {
-  render() {
-    const { onNextPage } = this.props;
-    if (!onNextPage) return null;
-    return (
-      <button
-        type="submit"
-        className={css.Button}
-        onClick={this.props.onNextPage}
-      >
-        Load more
-      </button>
-    );
-  }
-}
+const Button = onNextPage => {
+  if (!onNextPage) return null;
+  return (
+    <button
+      type="submit"
+      className={css.Button}
+      onClick={this.props.onNextPage}
+    >
+      Load more
+    </button>
+  );
+};
 
 Button.propTypes = {
   onNextPage: PropTypes.func,
 };
+
+export default Button;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,36 +1,39 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import s from './Modal.module.css';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+const Modal = ({ largeImageURL, onModalClose }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+  useEffect(() => {
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  handleKeyDown = event => {
+  const handleKeyDown = event => {
     if (event.keyCode === 27 || event.currentTarget === event.target) {
-      return this.props.onModalClose();
+      return onModalClose();
     }
   };
 
-  render() {
-    const { largeImageURL } = this.props;
-    return (
-      <div className={s.Overlay} onClick={this.handleKeyDown}>
-        <div className={s.Modal}>
-          <img src={largeImageURL} alt="" />
-        </div>
+  return (
+    <div className={s.Overlay} onClick={handleKeyDown}>
+      <div className={s.Modal}>
+        <img src={largeImageURL} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   onModalClose: PropTypes.func,
   largeImageURL: PropTypes.string.isRequired,
 };
+
+export default Modal;
